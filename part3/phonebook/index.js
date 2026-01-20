874f516a4d9fc86adf7ded3a60df08be74246300
Kelvin Cheng <kelvincheng2009@gmail.com>
2026-01-21 02:22:07 +1100

    removed reference to notes in phonebook index file

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -6,7 +6,7 @@ const app = express()
 
 const morgan = require('morgan')
 const person = require('./models/person')
-const note = require('../src/models/note')
+
 morgan.token('body', (req) => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
