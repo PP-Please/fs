e6ca3719acbba4f1c85604bcb1451eee305f5415
Kelvin Cheng <kelvincheng2009@gmail.com>
2026-01-21 18:42:26 +1100

    finished up to 4.7

---
+++ part4/blog/app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const blogsRouter = require('./controllers/blogs')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+const app = express()
+
+const mongoUrl = config.MONGODB_URI
+
+mongoose.connect(mongoUrl, { family: 4 })
+    .catch((err) => {
+        logger.error('failed to connect to MongoDB:', err.message)
+    })
+
+app.use(express.json())
+
+app.use('/api/blogs', blogsRouter)
+
+module.exports = app
---
+++ part4/blog/utils/list_helper.js
@@ -0,0 +1,64 @@
+const dummy = (blogs) => {
+    return 1
+}
+
+const totalLikes = (blogs) => {
+    const countLikes = (sum, item) => {
+        return sum + item.likes
+    }
+    return blogs.reduce(countLikes, 0)
+}
+
+const favouriteBlog = (blogs) => {
+    if (blogs.length === 0) return {}
+    return blogs.reduce((max, curr) => {
+        return (curr.likes > max.likes) ? curr : max
+    })
+}
+
+const mostBlogs = (blogs) => {
+    const counter = new Map()
+
+    for (let blog of blogs) {
+        const key = blog.author
+        if (counter.has(key)) {
+            counter.set(key, counter.get(key) + 1)
+        } else {
+            counter.set(key, 1)
+        }
+    }
+
+    const [author, num] = [...counter].reduce((a, b) => b[1] > a[1] ? b : a)
+    return {
+        author,
+        blogs: num
+    }
+}
+
+const mostLikes = (blogs) => {
+    const counter = new Map()
+
+    for (let blog of blogs) {
+        const key = blog.author
+        if (counter.has(key)) {
+            counter.set(key, counter.get(key) + blog.likes)
+        } else {
+            counter.set(key, blog.likes)
+        }
+    }
+
+    const [author, likes] = [...counter].reduce((a, b) => b[1] > a[1] ? b : a)
+    return {
+        author,
+        likes
+    }
+}
+
+module.exports = {
+    dummy,
+    totalLikes,
+    favouriteBlog,
+    mostBlogs,
+    mostLikes
+}
+
