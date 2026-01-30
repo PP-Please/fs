7fee93cbac5a26a4359aa8ff20cced79c2958659
Kelvin Cheng <kelvincheng2009@gmail.com>
2026-01-31 00:24:13 +1100

    finished up to 4.21

--- part4/blog/controllers/blogs.js
+++ part4/blog/controllers/blogs.js
@@ -30,8 +30,23 @@ blogsRouter.post('/', async(request, response) => {
 })
 
 blogsRouter.delete('/:id', async(request, response) => {
-    await Blog.findByIdAndDelete(request.params.id)
-    response.status(204).end()
+    const decodedToken = jwt.verify(request.token, process.env.SECRET)
+    if (!decodedToken.id) {
+        return response.status(401).json({ error: 'token missing or invalid' })
+    }
+
+    const user = await User.findById(decodedToken.id)
+    if (!user) return response.status(400).json({ error: 'missing or invalid user ID' })
+
+    const blog = await Blog.findById(request.params.id)
+    if (!blog) return response.status(404).json({ error: 'Blog not found' })
+
+    if (blog.user.toString() === user._id.toString()) {
+        await Blog.findByIdAndDelete(blog._id)
+        response.status(204).end()
+    } else {
+        response.status(403).json({ error: 'User not authorized to delete this blog' })
+    }
 })
 
 blogsRouter.put('/:id', async(request, response) => {
